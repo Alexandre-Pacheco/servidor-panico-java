ffa0f765a5d712baf78d1d6089591522755bbe43
Alexandre Pacheco <xandinho_petro@hotmail.com>
2025-08-05 20:40:56 -0300

    Update PendingState.java

--- src/main/java/br/ifpb/pdp/patterns/state/PendingState.java
+++ src/main/java/br/ifpb/pdp/patterns/state/PendingState.java
@@ -5,6 +5,7 @@ public class PendingState implements IncidentState {
     public PendingState(IncidentContext context) { this.context = context; }
     @Override public void confirm() { context.setState(new InProgressState(context)); }
     @Override public void cancel() { context.setState(new CancelledState(context)); }
-    @Override public void resolve() { /* Ação inválida */ }
-    @Override public void markAsFalse() { /* Ação inválida */ }
+    @Override public void resolve() {}
+    @Override public void markAsFalse() {}
+    @Override public String getStatusName() { return "Pendente"; }
 }
