ad97bb5b9858c7781facc096797aeaf7d8a3f7db
Alexandre Pacheco <xandinho_petro@hotmail.com>
2025-08-05 20:39:19 -0300

    Update IAuthService.java

--- src/main/java/br/ifpb/pdp/patterns/adapter/IAuthService.java
+++ src/main/java/br/ifpb/pdp/patterns/adapter/IAuthService.java
@@ -1,5 +1,11 @@
 package br.ifpb.pdp.patterns.adapter;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IAuthService {
     boolean login(String username, String password);
+    boolean addAdmin(String cpf, String password, String masterPassword, String name);
+    boolean removeAdmin(String cpf);
+    List<Map<String, String>> getAdmins();
 }
